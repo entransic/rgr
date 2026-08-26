debac1bdf4ef1d8f3c452457451f0961b6151eee
entransic <entransic@gmail.com>
2026-08-26 19:21:57 -0400

    added div with id as placeholder for graphs on index page

--- tests/indexPage.spec.ts
+++ tests/indexPage.spec.ts
@@ -120,13 +120,13 @@ test.describe("Basic tests for Home page", () => {
   })
 
   test("index page will have 4 unique articles within the section", async({page}) => {
-    for (let x=0; x<4; x++){
+    for (let x:number=0; x<4; x++){
       await expect (homePage.locator("section").nth(0).locator("article").nth(x)).toBeAttached();
     }
   })
 
   test("the 4 unique articles within the first section will have text", async({page}) => {
-    for (let x=0; x<4; x++){
+    for (let x:number=0; x<4; x++){
       await expect (homePage.locator("section").nth(0).locator("article").nth(x)).toContainText("ipsum");
     }
   })
@@ -136,9 +136,22 @@ test.describe("Basic tests for Home page", () => {
   })
 
   test("the 4 unique articles within the first section will have unique titles for the report types", async({page}) => {
-    let title = ["open/closed", "find rate", "fix rate", "by status"]
-    for (let x=0; x<4; x++){
+    let title:string = ["open/closed", "find rate", "fix rate", "by status"]
+    for (let x:number=0; x<4; x++){
       await expect (homePage.locator("section").nth(0).locator("article").nth(x).locator("h3")).toContainText(title[x]);
+    }
+  })
+
+  test("the 4 unique articles within the first section will have unique divs for the graph", async({page}) => {
+    for (let x:number=0; x<4; x++){
+      await expect (homePage.locator("section").nth(0).locator("article").nth(x).locator("div")).toBeAttached();;
+    }
+  })
+
+  test("the 4 unique articles within the first section will have unique divs for the graph with a unique icss id", async({page}) => {
+    let id:string = ["graph-open-closed", "graph-find-rate", "graph-fix-rate", "graph-by-status"]
+    for (let x=0; x<4; x++){
+      await expect (homePage.locator("section").nth(0).locator("article").nth(x).locator("div")).toHaveId(id[x]);
     }
   })
 
