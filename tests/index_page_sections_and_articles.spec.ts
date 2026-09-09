7ca6e847f7b0621ec06d843be2c13b0699d35aea
entransic <entransic@gmail.com>
2026-09-09 17:52:02 -0400

    updated graph width and height to 600 x 300

--- tests/index_page_sections_and_articles.spec.ts
+++ tests/index_page_sections_and_articles.spec.ts
@@ -86,15 +86,15 @@ test.describe("Basic tests for Home page", () => {
     }
   });
 
-  test("the 4 unique articles within the first section will have unique divs with width of 200px", async ({ page }) => {
+  test("the 4 unique articles within the first section will have unique divs with width of 600px", async ({ page }) => {
     for (let x: number = 0; x < reportTitle.length; x++) {
-      await expect(homePage.locator("section").nth(0).locator("article").nth(x).locator("div")).toHaveCSS("width", "300px");
+      await expect(homePage.locator("section").nth(0).locator("article").nth(x).locator("div")).toHaveCSS("width", "600px");
     }
   });
 
-  test("the 4 unique articles within the first section will have unique divs with height of 100px", async ({ page }) => {
+  test("the 4 unique articles within the first section will have unique divs with height of 300px", async ({ page }) => {
     for (let x: number = 0; x < reportTitle.length; x++) {
-      await expect(homePage.locator("section").nth(0).locator("article").nth(x).locator("div")).toHaveCSS("height", "200px");
+      await expect(homePage.locator("section").nth(0).locator("article").nth(x).locator("div")).toHaveCSS("height", "300px");
     }
   });
 
