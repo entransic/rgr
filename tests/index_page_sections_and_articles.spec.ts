16c7ae893d05d04d4b98cb03aa437be31809a254
entransic <entransic@gmail.com>
2026-09-09 13:20:11 -0400

    refactored, ran prettier, and added css class for report graphs

--- tests/index_page_sections_and_articles.spec.ts
+++ tests/index_page_sections_and_articles.spec.ts
@@ -7,14 +7,17 @@ let homeURL: string = "/";
 let aboutURL: string = "/about";
 let searchURL: string = "/search";
 let submitURL: string = "/submit";
+let reportTitle: string[] = ["open/closed", "find rate", "fix rate", "by status"];
+let reportArticleID: string[] = ["report-open-closed", "report-find-rate", "report-fix-rate", "report-by-status"];
+let graphID: string[] = ["graph-open-closed", "graph-find-rate", "graph-fix-rate", "graph-by-status"];
 
 test.describe("Basic tests for Home page", () => {
   test.describe.configure({ mode: "parallel" });
 
-//
-// BEFORE EACH SETUP
-// OPEN BROWSER AND GOTO INDEXPAGE
-//
+  //
+  // BEFORE EACH SETUP
+  // OPEN BROWSER AND GOTO INDEX PAGE
+  //
 
   test.beforeEach(async ({ browser }) => {
     sharedContext = await browser.newContext();
@@ -24,7 +27,7 @@ test.describe("Basic tests for Home page", () => {
   });
 
   //
-  // TESTS 
+  // TESTS
   //
 
   test("index page will have a <section> element within <main> for description of app", async ({ page }) => {
@@ -35,57 +38,62 @@ test.describe("Basic tests for Home page", () => {
     await expect(homePage.locator("section").filter({ has: homePage.locator("article") })).toBeAttached();
   });
 
-  test("index page will have at least 4 articles within the section", async({page}) => {
-    await expect (homePage.locator("section").nth(0).locator("article").nth(3)).toBeAttached();
-  })
+  test("index page will have at least 4 articles within the section", async ({ page }) => {
+    await expect(homePage.locator("section").nth(0).locator("article").nth(3)).toBeAttached();
+  });
 
-  test("index page will have 4 unique articles within the section", async({page}) => {
-    for (let x:number=0; x<4; x++){
-      await expect (homePage.locator("section").nth(0).locator("article").nth(x)).toBeAttached();
-    }
-  })
-
-  test("the 4 unique articles within the first section will have text", async({page}) => {
-    for (let x:number=0; x<4; x++){
-      await expect (homePage.locator("section").nth(0).locator("article").nth(x)).toContainText("ipsum");
-    }
-  })
-
-  test("the first section will have a h2 with text bug status", async({page}) => {
-      await expect (homePage.locator("section").nth(0).locator("h2").nth(0)).toContainText("bug status");
-  })
-
-  test("the 4 unique articles within the first section will have unique titles for the report types", async({page}) => {
-    let title:string = ["open/closed", "find rate", "fix rate", "by status"]
-    for (let x:number=0; x<4; x++){
-      await expect (homePage.locator("section").nth(0).locator("article").nth(x).locator("h3")).toContainText(title[x]);
-    }
-  })
-
-  test("the 4 unique articles within the first section will have unique divs for the graph", async({page}) => {
-    for (let x:number=0; x<4; x++){
-      await expect (homePage.locator("section").nth(0).locator("article").nth(x).locator("div")).toBeAttached();;
-    }
-  })
-
-  test("the 4 unique articles within the first section will have unique divs for the graph with a unique icss id", async({page}) => {
-    let id:string = ["graph-open-closed", "graph-find-rate", "graph-fix-rate", "graph-by-status"]
-    for (let x=0; x<4; x++){
-      await expect (homePage.locator("section").nth(0).locator("article").nth(x).locator("div")).toHaveId(id[x]);
-    }
-  })
-
-  test("the 4 unique articles within the first section will have unique ids for the reports", async({page}) => {
-    let id:string = ["report-open-closed", "report-find-rate", "report-fix-rate", "report-by-status"]
-    for (let x=0; x<4; x++){
-      await expect (homePage.locator("section").nth(0).locator("article").nth(x)).toHaveId(id[x]);
+  test("index page will have 4 unique articles within the section", async ({ page }) => {
+    for (let x: number = 0; x < reportTitle.length; x++) {
+      await expect(homePage.locator("section").nth(0).locator("article").nth(x)).toBeAttached();
     }
   });
-//
-// AFTER ALL TEARDOWN 
-//
+
+  test("the 4 unique articles within the first section will have text", async ({ page }) => {
+    for (let x: number = 0; x < reportTitle.length; x++) {
+      await expect(homePage.locator("section").nth(0).locator("article").nth(x)).toContainText("ipsum");
+    }
+  });
+
+  test("the first section will have a h2 with text bug status", async ({ page }) => {
+    await expect(homePage.locator("section").nth(0).locator("h2").nth(0)).toContainText("bug status");
+  });
+
+  test("the 4 unique articles within the first section will have unique titles for the report types", async ({ page }) => {
+    for (let x: number = 0; x < reportTitle.length; x++) {
+      await expect(homePage.locator("section").nth(0).locator("article").nth(x).locator("h3")).toContainText(reportTitle[x]);
+    }
+  });
+
+  test("the 4 unique articles within the first section will have unique divs for the graph", async ({ page }) => {
+    for (let x: number = 0; x < reportTitle.length; x++) {
+      await expect(homePage.locator("section").nth(0).locator("article").nth(x).locator("div")).toBeAttached();
+    }
+  });
+
+  test("the 4 unique articles within the first section will have unique divs with CSS for graphs", async ({ page }) => {
+    for (let x: number = 0; x < reportTitle.length; x++) {
+      await expect(homePage.locator("section").nth(0).locator("article").nth(x).locator("div")).toContainClass("report");
+    }
+  });
+
+  test("the 4 unique articles within the first section will have unique divs for the graph with a unique css id", async ({
+    page,
+  }) => {
+    for (let x = 0; x < reportTitle.length; x++) {
+      await expect(homePage.locator("section").nth(0).locator("article").nth(x).locator("div")).toHaveId(graphID[x]);
+    }
+  });
+
+  test("the 4 unique articles within the first section will have unique ids for the reports", async ({ page }) => {
+    for (let x = 0; x < reportTitle.length; x++) {
+      await expect(homePage.locator("section").nth(0).locator("article").nth(x)).toHaveId(reportArticleID[x]);
+    }
+  });
+
+  //
+  // AFTER ALL TEARDOWN
+  //
   test.afterAll(async () => {
     await sharedContext.close();
   });
 });
-
