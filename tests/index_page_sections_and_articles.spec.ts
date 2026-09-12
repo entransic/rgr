6c041765b4fc9c8d626e964c914cbfc623cbba62
entransic <entransic@gmail.com>
2026-09-12 16:50:16 -0400

    added combobox  containing projects to select

--- tests/index_page_sections_and_articles.spec.ts
+++ tests/index_page_sections_and_articles.spec.ts
@@ -9,7 +9,8 @@ let searchURL: string = "/search";
 let submitURL: string = "/submit";
 let reportTitle: string[] = ["open/closed", "find rate", "fix rate", "by status"];
 let reportArticleID: string[] = ["report-open-closed", "report-find-rate", "report-fix-rate", "report-by-status"];
-let graphID: string[] = ["graph-open-closed", "graph-find-rate", "graph-fix-rate", "graph-by-status"];
+let graphID: string[] = ["graph-open-closed", "graph-find-rate", "graph-fix-rate", "graph-by-status"]
+let projects = ["Netscape Navigator", "AOL Navigator", "Mozilla Phoenix"]
 
 test.describe("Basic tests for Home page", () => {
   test.describe.configure({ mode: "parallel" });
@@ -32,6 +33,24 @@ test.describe("Basic tests for Home page", () => {
 
   test("index page will have a <section> element within <main> for description of app", async ({ page }) => {
     await expect(homePage.locator("main").filter({ has: homePage.locator("section") })).toBeAttached();
+  });
+
+  test("index page will have a label for selecting a project from a drop down list`", async ({ page }) => {
+    await expect(homePage.locator("main").filter({ has: homePage.locator("label") })).toBeAttached();
+  });
+  
+  test("the label for selecting a project from a drop down list will be 'Select a project'", async ({ page }) => {
+    await expect(homePage.locator("main").filter({ has: homePage.locator("label") })).toContainText("Select a project");
+  });
+  
+  test("the list of projects will include all the browsers", async ({ page }) => {
+      await expect(homePage.locator("option")).toHaveCount(Number(projects.length));
+  });
+  
+  test("the list of projects in dropdown will contain the name of browsers", async ({ page }) => {
+    for (let x; x < projects.length; x++) {
+      await expect(homePage.locator("option").nth(x)).toContainText(projects[x]);
+    }
   });
 
   test("index page will have an <article> element within <section> for description text", async ({ page }) => {
@@ -82,7 +101,10 @@ test.describe("Basic tests for Home page", () => {
     await homePage.setViewportSize({ width: 1280, height: 1080 });
     await expect(homePage.locator("section").nth(0)).toHaveCSS("display", "flex");
   });
-
+  //
+  // END MEDIA WIDTH TEST
+  //
+  
   test("the 4 unique articles within the first section will have unique titles for the report types", async ({ page }) => {
     for (let x: number = 0; x < reportTitle.length; x++) {
       await expect(homePage.locator("section").nth(0).locator("article").nth(x).locator("h3")).toContainText(reportTitle[x]);
